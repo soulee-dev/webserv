35d1073c2c304338a723dccaf4e236320c0e977f
soulee <me@soulee.dev>
2023-07-12 16:56:43 +0900

    Add tiny webserv

--- tiny.c
+++ tiny.c
@@ -1,7 +1,6 @@
 #include "csapp.h"
 
 void	doit(int fd);
-// What is rio_t?
 void	read_requesthdrs(rio_t *rp);
 int		parse_uri(char *uri, char *filename, char *cgiargs);
 void	serve_static(int fd, char *filename, int filesize);
@@ -28,7 +27,65 @@ int	main(int argc, char **argv)
 	{
 		clientlen = sizeof(clientaddr);
 		connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-		getnameinfo((SA *) &clientaddr, clients)
+		Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+		printf("Accepted connection from (%s, %s)\n", hostname, port);
+		doit(connfd);
+		Close(connfd);
 	}
-	
+}
+
+void	doit(int fd)
+{
+	int			is_static;
+	struct stat	sbuf;
+	char		buf[MAXLINE];
+	char		method[MAXLINE];
+	char		uri[MAXLINE];
+	char		version[MAXLINE];
+	char		filename[MAXLINE];
+	char		cgiargs[MAXLINE];
+	rio_t		rio;
+
+	Rio_readinitb(&rio, fd);
+	Rio_readlineb(&rio, buf, MAXLINE);
+	printf("Request headers:\n");
+	printf("%s", buf);
+	sscanf(buf, "%s %s %s", method, uri, version);
+	if (strcasecmp(method, "GET"))
+	{
+		clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+		return ;
+	}
+	read_requesthdrs(&rio);
+
+	is_static = parse_uri(uri, filename, cgiargs);
+	if (stat(filename, &sbuf) < 0)
+	{
+		clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+		return ;
+	}
+	if (is_static)
+	{
+		// Serve static content
+		if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode))
+		{
+			clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+			return ;
+		}
+	}
+	else
+	{
+		if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode))
+		{
+			clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
+			return ;
+		}
+		serve_dynamic(fd, filename, cgiargs);
+	}
+}
+
+void	clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+{
+	char	buf[MAXLINE];
+	char	body[MAXBUF];
 }
