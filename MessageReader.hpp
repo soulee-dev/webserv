d11beaccf52963986fe0d67c58f14d3845d22cee
jaemjeon <piepie1346@gmail.com>
2023-07-25 23:00:18 +0900

    http pipelining때문에 결국 reader에서 입력버퍼랑 메시지버퍼를 같이 가지고 있어야함...

--- MessageReader.hpp
+++ MessageReader.hpp
@@ -15,6 +15,8 @@ class MessageReader
 public:
 	MessageReader() {};
 	~MessageReader() {};
+	std::map<int, std::vector<unsigned char>> readBuffer;
+	std::map<int, Message> messageBuffer;
 	virtual void readStartLine(const char *buffer, Message &message) = 0;
 	virtual void readHeader(const char *buffer, Message &message);
 	virtual void readBody(const char *buffer, Message &message);
