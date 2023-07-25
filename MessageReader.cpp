8d263e552d794189b5db069a67168f8ec4f22d07
jaemjeon <piepie1346@gmail.com>
2023-07-25 23:07:33 +0900

    readBuffer MessageBuffer를 고려해 함수 수정

--- MessageReader.cpp
+++ MessageReader.cpp
@@ -1,42 +1,44 @@
 #include "MessageReader.hpp"
 
-void MessageReader::readHeader(const char *buffer, Message &message)
+void MessageReader::readHeader(const char *buffer, int client_fd)
 {
 	std::stringstream sstream;
 	std::stringstream headerSstream;
 	std::string line;
 	std::string key;
 	std::string value;
+	Message currMessage = messageBuffer[client_fd];
 
-	message.raw.insert(message.raw.end(), buffer, buffer + strlen(buffer));
-	sstream << buffer;
+	readBuffer[client_fd] << buffer;
 	while (getline(sstream, line, '\n'))
 	{
 		if (*line.end() == '\r')
 			line.pop_back();
 		if (line.empty())
 		{
-			message.parseState = BODY;
+			currMessage.parseState = BODY;
 			break;
 		}
 		headerSstream << line;
 		getline(headerSstream, key, ':');
 		getline(headerSstream, value);
-		message.headers[key] = value;
+		currMessage.headers[key] = value;
 	}
 }
 
-void MessageReader::readBody(const char *buffer, Message &message)
+void MessageReader::readBody(const char *buffer, int client_fd)
 {
 	std::stringstream sstream;
 	std::string line;
+	Message currMessage = messageBuffer[client_fd];
 
-	message.raw.insert(message.raw.end(), buffer, buffer + strlen(buffer));
+	readBuffer[client_fd] << buffer;
 	while (getline(sstream, line, '\n'))
 	{
 		if (line.empty())
 		{
-			message.parseState = DONE;
+			currMessage.body = std::vector<unsigned char>(readBuffer[client_fd].str().begin(), readBuffer[client_fd].str().end());
+			currMessage.parseState = DONE;
 			return ;
 		}
 	}
--- MessageReader.hpp
+++ MessageReader.hpp
@@ -15,10 +15,10 @@ class MessageReader
 public:
 	MessageReader() {};
 	~MessageReader() {};
-	std::map<int, std::vector<unsigned char>> readBuffer;
+	std::map<int, std::stringstream> readBuffer;
 	std::map<int, Message> messageBuffer;
-	virtual void readStartLine(const char *buffer, Message &message) = 0;
-	virtual void readHeader(const char *buffer, Message &message);
-	virtual void readBody(const char *buffer, Message &message);
+	virtual void readStartLine(const char *buffer, int client_fd) = 0;
+	virtual void readHeader(const char *buffer, int client_fd);
+	virtual void readBody(const char *buffer, int client_fd);
 private:
 };
