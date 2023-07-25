3aa6bcfa4403acb547e80e487b4b7d9a3d97553b
jaemjeon <piepie1346@gmail.com>
2023-07-25 22:00:34 +0900

    readstate enum을 밖에 빼서 전역으로 사용. state는 메시지마다 달기

--- MessageReader.hpp
+++ MessageReader.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
+enum State
+{
+	START,
+	HEADER,
+	BODY,
+	DONE
+};
+
 class MessageReader
 {
 public:
 	MessageReader();
 	~MessageReader();
-	int state;
-	enum State
-	{
-		START,
-		HEADER,
-		BODY,
-		DONE
-	};
 	static void readStartLine(const char *buffer);
 	static void readHeader(const char *buffer);
 	static void readBody(const char *buffer);
