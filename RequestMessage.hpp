46aa1d6c10b09620037ec1a3fea4ba0fe7ac12cc
jaemjeon <piepie1346@gmail.com>
2023-07-25 21:41:18 +0900

    add RequestMessage struct

---
+++ RequestMessage.hpp
@@ -0,0 +1,11 @@
+#include "Message.hpp"
+
+class RequestMessage : public Message
+{
+public:
+	RequestMessage() {};
+	~RequestMessage() {};
+	std::string method;
+	std::string requestTarget;
+	std::string httpVersion;
+};
