be210b70a91073e48a99cc5b10e416cbb7930295
Jihee Nam <jinam@c1r3s4.42seoul.kr>
2023-07-27 16:27:10 +0900

    split parse_func to Location.cpp and Server.cpp

--- Location.cpp
+++ Location.cpp
@@ -1,49 +1,16 @@
 #include "Location.hpp"
 #include "Server.hpp"
 #include "config.hpp"
-Server::Server()
-    : listen(80), serverName(""), root("/"), autoIndex(false), clientBodySize(10240)
+#include <sstream>
+
+Location::Location()
+    : root("/"), allowMethod(0), autoIndex(false), clientBodySize(10240)
 {
 }
 
-Server::~Server()
+Location::~Location()
 {
 }
-
-bool Server::fillServer(std::map<std::string, Location>& mapLocations, std::map<std::string, std::string>& mapSentence)
-{
-    std::map<std::string, std::string>::iterator it = mapSentence.begin();
-
-    this->locations = mapLocations; // 적어도 하나이상의 locationBlock이 있어야 한다.
-    while (it != mapSentence.end())
-    {
-        if (it->first == "listen")
-            this->setListen(it->second);
-        else if (it->first == "server_name")
-            this->setServerName(it->second);
-        else if (it->first == "root")
-            this->setRoot(it->second);
-        else if (it->first == "error_page")
-        {
-            if (this->setErrorPage(it->second))
-                return true;
-        }
-        else if (it->first == "return")
-        {
-            if (this->setRedirection(it->second))
-                return true;
-        }
-        else if (it->first == "autoindex")
-            this->setAutoIndex(it->second);
-        else if (it->first == "client_max_body_size")
-            this->setClientBodySize(it->second);
-        else
-            return true;
-        it++;
-    }
-    return false;
-}
-
 bool Location::fillLocationBlock(std::map<std::string, std::string>& mapSentence)
 {
     std::map<std::string, std::string>::iterator it = mapSentence.begin();
@@ -78,80 +45,6 @@ bool Location::fillLocationBlock(std::map<std::string, std::string>& mapSentence
         it++;
     }
     return false;
-}
-void Server::setListen(std::string& input)
-{
-    long parsedPort = strtod(input.c_str(), NULL);
-
-    if (parsedPort < 0 || parsedPort > 65535)
-        std::cout << "Warning : invalid port number" << std::endl;
-    this->listen = parsedPort;
-}
-
-void Server::setServerName(std::string& input)
-{
-    this->serverName = input;
-}
-
-void Server::setRoot(std::string& input)
-{
-    this->root = input;
-}
-
-bool Server::setErrorPage(std::string const& sentence)
-{
-    std::stringstream sstream;
-    std::vector<std::string> tokens;
-    std::string token;
-    std::vector<int> vecErrorCode;
-    std::string errorPath;
-
-    sstream << sentence;
-    while (sstream >> token)
-        tokens.push_back(token);
-
-    if (tokens.size() < 2)
-        return true;
-    errorPath = tokens.back();
-    tokens.pop_back();
-
-    for (size_t i = 0; i < tokens.size(); i++)
-        vecErrorCode.push_back(std::strtod(tokens[i].c_str(), NULL));
-    this->errorPage.insert(std::pair<std::vector<int>, std::string>(vecErrorCode, errorPath));
-    return false;
-}
-
-bool Server::setRedirection(std::string const& sentence)
-{
-    std::stringstream sstream;
-    std::vector<std::string> tokens;
-    std::string token;
-    std::string redirectionPath;
-    int redirectionCode;
-
-    sstream << sentence;
-    while (sstream >> token)
-        tokens.push_back(token);
-
-    if (tokens.size() != 2)
-        return true;
-    redirectionCode = std::strtod(tokens[0].c_str(), NULL);
-    redirectionPath = tokens[1];
-    this->redirection[redirectionPath] = redirectionCode;
-    return false;
-}
-
-void Server::setAutoIndex(std::string& input)
-{
-    if (input == "on")
-        this->autoIndex = true;
-    else
-        this->autoIndex = false;
-}
-
-void Server::setClientBodySize(std::string& input)
-{
-    this->clientBodySize = std::strtod(input.c_str(), NULL);
 }
 
 void Location::setRoot(std::string& input)
--- Server.hpp
+++ Server.hpp
@@ -1,31 +1,33 @@
 #pragma once
-#include <map>
-#include "Location.hpp"
 
+#include "Location.hpp"
+#include <map>
+#include <string>
 class Server
 {
 public:
-    Server() : listen(80), serverName(""), root("/"), autoIndex(false), clientBodySize(10240) {};
-    ~Server() {};
-    bool fillServer(std::map<std::string, Location> &mapLocations, std::map<std::string, std::string> &mapSentence);
-    int getListen() const { return this->listen; };
-    std::string getServerName() const { return this->serverName; };
+    Server();
+    ~Server();
+    bool fillServer(std::map<std::string, Location>& mapLocations, std::map<std::string, std::string>& mapSentence);
+    int getListen() const;
+    std::string getServerName() const;
     void DebugPrint();
+
 private:
     int listen;
     std::string serverName;
     std::string root;
     std::map<std::vector<int>, std::string> errorPage;
-    std::map<std::string, int>redirection;
+    std::map<std::string, int> redirection;
     bool autoIndex;
     size_t clientBodySize;
     std::map<std::string, Location> locations;
 
-    void setListen(std::string &input);
-    void setServerName(std::string &input);
-    void setRoot(std::string &input);
-    bool setErrorPage(std::string const &sentence);
-    bool setRedirection(std::string const &sentence);
-    void setAutoIndex(std::string &input);
-    void setClientBodySize(std::string &input);
+    void setListen(std::string& input);
+    void setServerName(std::string& input);
+    void setRoot(std::string& input);
+    bool setErrorPage(std::string const& sentence);
+    bool setRedirection(std::string const& sentence);
+    void setAutoIndex(std::string& input);
+    void setClientBodySize(std::string& input);
 };
