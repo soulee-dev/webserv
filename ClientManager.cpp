b741ef57fbc4dc0e74c3846240ed4041cc9d7d09
jaemjeon <piepie1346@gmail.com>
2023-08-02 17:00:37 +0900

    reinterpret_cast 수정

--- ClientManager.cpp
+++ ClientManager.cpp
@@ -31,20 +31,20 @@ void ClientManager::disconnectClient(SOCKET client_fd)
 
 bool ClientManager::readEventProcess(struct kevent& currEvent)
 {
-    Client &currClient = reinterpret_cast<Client&>(currEvent.udata);
-    if (currClient.readMessage())
+    Client* currClient = reinterpret_cast<Client*>(currEvent.udata);
+    if (currClient->readMessage()) // read에서 에러나면 true를 반환
         disconnectClient(currEvent.ident);
-    if (currClient.readEventProcess()) // 응답을 보낼 준비가 되면 true를 반환
+    if (currClient->readEventProcess()) // 응답을 보낼 준비가 되면 true를 반환
         return true;
     return false;
 }
 
 bool ClientManager::writeEventProcess(struct kevent& currEvent)
 {
-    Client &currClient = reinterpret_cast<Client&>(currEvent.udata);
-    if (currClient.writeEventProcess()) // write에 실패하면 true를 반환
+    Client* currClient = reinterpret_cast<Client*>(currEvent.udata);
+    if (currClient->writeEventProcess()) // write에 실패하면 true를 반환
         disconnectClient(currEvent.ident);
-    if (currClient.isSendBufferEmpty())
+    if (currClient->isSendBufferEmpty())
         return true;
     return false;
 }
