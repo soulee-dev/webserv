2abda1bed2b721c9042dc3f0f0d194cca6bdbe69
jaemjeon <jaemjeon@student.42seoul.kr>
2023-08-06 02:33:33 +0900

    config파일을 root의 config폴더로 빼고 인자가 없을시 default.conf를 찾아가도록함

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,15 +4,19 @@
 
 int main(int argc, char** argv)
 {
+    std::string configFileName;
+
     if (argc != 2)
     {
-        std::cout << "please input one config file" << std::endl;
-        exit(1);
+        std::cout << "use default config file..." << std::endl;
+        configFileName = "./config/default.conf";
     }
+    else
+        configFileName = argv[1];
     ConfigParser configParser;
     ServerManager serverManager;
 
-    configParser.parseConfig(argv[1]);
+    configParser.parseConfig(configFileName);
     serverManager.setServers(configParser.server);
 
     serverManager.initServers();
