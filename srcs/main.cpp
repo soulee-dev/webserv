87d9bf9f6193efe121f1a621845fb1af16723633
jaemjeon <jaemjeon@student.42seoul.kr>
2023-08-11 17:19:59 +0900

    fix argc error

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv)
         std::cout << "please input only one file..." << std::endl;
         return 1;
     }
-    if (argc == 0)
+    if (argc == 1)
     {
         std::cout << "use default config file..." << std::endl;
         configFileName = "./config/default.conf";
