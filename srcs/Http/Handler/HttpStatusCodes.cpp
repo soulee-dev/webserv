34119c2ae58a8b3fe0c97e7e353004edd8c2e707
Soul Lee <alus20x@gmail.com>
2023-08-14 22:25:05 +0900

    Add status_code for 201

--- srcs/Http/Handler/HttpStatusCodes.cpp
+++ srcs/Http/Handler/HttpStatusCodes.cpp
@@ -6,6 +6,7 @@ std::map<int, std::string> get_status_codes()
     if (STATUS_CODES.empty())
 	{
         STATUS_CODES[200] = "OK";
+        STATUS_CODES[201] = "Created";
 		STATUS_CODES[403] = "Forbidden";
 		STATUS_CODES[404] = "Not Found";
         STATUS_CODES[405] = "Method Not Allowed";
