c6ab67f3a174976650b6d45685283e6dc96d8b2f
jaemjeon <jaemjeon@student.42seoul.kr>
2023-08-29 16:23:52 +0900

    redirect status code add

--- srcs/Http/Handler/HttpStatusCodes.cpp
+++ srcs/Http/Handler/HttpStatusCodes.cpp
@@ -7,6 +7,7 @@ std::map<int, std::string> get_status_codes()
 	{
         STATUS_CODES[200] = "OK";
         STATUS_CODES[201] = "Created";
+        STATUS_CODES[301] = "Redirect";
         STATUS_CODES[400] = "Bad Request";
 		STATUS_CODES[403] = "Forbidden";
 		STATUS_CODES[404] = "Not Found";
